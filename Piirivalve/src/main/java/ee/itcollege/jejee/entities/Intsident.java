c949e5cb810f8ccce34751bb121357c15f8d7896
siimav <siim.aaver@gmail.com>
2011-11-14 14:53:55 +0200

    entity 2

---
+++ Piirivalve/src/main/java/ee/itcollege/jejee/entities/Intsident.java
@@ -0,0 +1,11 @@
+package ee.itcollege.jejee.entities;
+
+import org.springframework.roo.addon.entity.RooEntity;
+import org.springframework.roo.addon.javabean.RooJavaBean;
+import org.springframework.roo.addon.tostring.RooToString;
+
+@RooJavaBean
+@RooToString
+@RooEntity
+public class Intsident {
+}
